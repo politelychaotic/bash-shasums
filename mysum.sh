70a744e34d98575a0b59fbd545a037bc7e6ba9dc
bash.girlie <94557891+politelychaotic@users.noreply.github.com>
2025-07-28 09:43:22 -0500

    Update mysum.sh

--- mysum.sh
+++ mysum.sh
@@ -15,5 +15,9 @@ if [ ! -f "$fname" ]; then
 fi
 
 sha256_sum=$(shasum -a 256 "$fname" | awk '{print $1}');
+sha512_sum=$(shasum -a 512 "$fname" | awk '{print $1}');
+md5_sum=$(md5sum "$fname" | awk '{print $1}');
 
-echo "SHA256 checksum: '$fname': $sha256_sum";
+echo -e "\nSHA512 checksum: '$fname': $sha512_sum";
+echo -e "\nSHA256 checksum: '$fname': $sha256_sum";
+echo -e "\nMD5 checksum: '$fname': $md5_sum";
